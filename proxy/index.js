3a11bfc49147beec0a1ce25a6a4ec05ac6a759a9
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-28 09:16:31 +0100

    Modified

--- proxy/index.js
+++ proxy/index.js
@@ -2,10 +2,15 @@
  * index.js
  */
 var ProxyConfigurations = require(__dirname+'/configurations.js');
+var ProxyErrors = require(__dirname+'/errors.js');
 var ProxyLibraries = require(__dirname+'/libraries.js');
 var ProxyMappings = require(__dirname+'/mappings.js');
 var ProxyPaths = require(__dirname+'/paths.js');
+var ProxyQueries = require(__dirname+'/queries.js');
 var ProxyResources = require(__dirname+'/resources.js');
+var ProxySchemas = require(__dirname+'/schemas.js');
+var ProxyTypes = require(__dirname+'/types.js');
+var ProxyUtilities = require(__dirname+'/utilities.js');
 
 /**
  * Create a new Proxy that let users create sub-proxies.
@@ -22,20 +27,20 @@ Proxy.prototype.configurations = function() {
 }
 
 /**
+ * Create a new ProxyErrors object.
+ * @return {ProxyErrors}
+ */
+Proxy.prototype.errors = function() {
+  return new ProxyErrors();
+}
+
+/**
  * Create a new ProxyLibraries object.
  * @return {ProxyLibraries}
  */
  
 Proxy.prototype.libraries = function() {
   return new ProxyLibraries();
-}
-
-/**
- * Create a new ProxyPaths object.
- * @return {ProxyPaths}
- */
-Proxy.prototype.paths = function() {
-  return new ProxyPaths();
 }
 
 /**
@@ -47,11 +52,51 @@ Proxy.prototype.mappings = function() {
 }
 
 /**
+ * Create a new ProxyPaths object.
+ * @return {ProxyPaths}
+ */
+Proxy.prototype.paths = function() {
+  return new ProxyPaths();
+}
+
+/**
+ * Create a new ProxyQueries object.
+ * @return {ProxyQueries}
+ */
+Proxy.prototype.queries = function() {
+  return new ProxyQueries();
+}
+
+/**
  * Create a new ProxyResources object.
  * @return {ProxyResources}
  */
 Proxy.prototype.resources = function() {
   return new ProxyResources();
+}
+
+/**
+ * Create a new ProxySchemas object.
+ * @return {ProxySchemas}
+ */
+Proxy.prototype.schemas = function() {
+  return new ProxySchemas();
+}
+
+/**
+ * Create a new ProxyTypes object.
+ * @return {ProxyTypes}
+ */
+Proxy.prototype.types = function() {
+  return new ProxyTypes();
+}
+
+/**
+ * Create a new ProxyUtilities object.
+ * @return {ProxyUtilities}
+ */
+Proxy.prototype.utilities = function() {
+  return new ProxyUtilities();
 }
 
 //ORIGINAL module.exports = new Proxy();
